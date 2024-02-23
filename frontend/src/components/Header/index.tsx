d1798a08ccde35697ac5e3d2c74a3ea0b1fb13de
Tomás <tomas153lm@gmail.com>
2024-02-23 15:17:27 -0300

    feature: add header feature, just visual

---
+++ frontend/src/components/Header/index.tsx
@@ -0,0 +1,27 @@
+import Searchbar from '../Searchbar';
+import Icon from '../../assets/Icon';
+// import { User } from 'lucide-react';
+import { Link } from 'react-router-dom';
+import './index.css';
+
+export default function Header() {
+  return (
+    <header className="w-full-screen bg-blue-500 p-5">
+      <div className='flex gap-3 items-center'>
+        <Icon />
+        <Searchbar />
+        <div className='flex justify-center gap-3'>
+          {/* <User className='bg-white rounded-full user-icon p-3' /> */}
+          <Link to="/login" className='hover:underline'>Login</Link>
+          <Link to="/register" className='hover:underline'>Register</Link>
+        </div>
+      </div>
+      <nav className='mt-3'>
+        <ul className='flex gap-3 justify-center'>
+          <Link to="/products" className='hover:underline'>Products</Link>
+          <Link to="/tags" className='hover:underline'>Tags</Link>
+        </ul>
+      </nav>
+    </header>
+  );
+}
